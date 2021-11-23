776356f72bdea3be40d913eb19e37e44cefcb3bb
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2021-11-23 14:38:36 +0700

    mengubah spasi menjadi ~, newline menjadi ;, mengabaikan trailing space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cyk import *
 v, t = bacaGrammar('chowsky')
-r = 'v="abdsn"'
+with open('tc1.txt', 'r') as file:
+    r = file.read().replace('\n', ';').replace(' ','~')
 ta = cyk(v, t, r)
 tunjukkanHasil(ta, r)
