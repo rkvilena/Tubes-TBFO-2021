86e516c0adb6c59d4ada9e845cdaac67d96316ca
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2021-11-23 19:10:34 +0700

    perbaikan grammar def

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from cyk import *
 v, t = bacaGrammar('chowsky')
 with open('tc1.txt', 'r') as file:
     r = file.read().replace('\n', ';').replace(' ','~')
+#r='def~jun():;x=2'
 print(r)
-
 
 ta = cyk(v, t, r)
 tunjukkanHasil(ta, r)
