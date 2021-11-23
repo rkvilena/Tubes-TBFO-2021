0e8b89916c5558011cbfd39b38e42dc2d2518406
rkvilena <rwad0711@gmail.com>
2021-11-23 16:00:14 +0700

    Finishing (hopefully), for loop.

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from cyk import *
 v, t = bacaGrammar('chowsky')
 with open('tc1.txt', 'r') as file:
     r = file.read().replace('\n', ';').replace(' ','~')
+print(r)
+
+
 ta = cyk(v, t, r)
 tunjukkanHasil(ta, r)
